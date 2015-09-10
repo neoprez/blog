c53fa4fe15ddc486267452123fd51c6a739ae07e
Rodny Perez <rodnyeperez@gmail.com>
2015-09-10 00:28:26 -0400

    Add save articles into database

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,6 +2,15 @@ class ArticlesController < ApplicationController
 	def new
 	end
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(article_params)
+
+		@article.save
+		redirect_to @article
 	end
+
+	private
+		def article_params
+			# This is often factored out into its own method so it can be reused by multiple actions in the same controller, for example create and update.
+			params.require(:article).permit(:title, :text)
+		end
 end
